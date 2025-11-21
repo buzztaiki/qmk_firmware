24597c8b0615f189647de312846404608f05747e
Taiki Sugawara <buzz.taiki@gmail.com>
2025-11-21 22:13:21 +0900

    define choral hold layout but dont use it

--- keyboards/yushakobo/ergo68/keymaps/buzztaiki/config.h
+++ keyboards/yushakobo/ergo68/keymaps/buzztaiki/config.h
@@ -1,6 +1,4 @@
 #pragma once
 
-// https://docs.qmk.fm/tap_hold
-// https://www.reddit.com/r/ErgoMechKeyboards/comments/1bt3nf5/hold_keys_like_hrm_space_significantly_reduce_my/
-// 全部のキーじゃなくて、親指系だけ有効がよいかも。
 #define HOLD_ON_OTHER_KEY_PRESS
+/* #define CHORDAL_HOLD */
--- keyboards/yushakobo/ergo68/keymaps/buzztaiki/keymap.c
+++ keyboards/yushakobo/ergo68/keymaps/buzztaiki/keymap.c
@@ -17,6 +17,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+    LAYOUT(
+        'L', 'L', 'L', 'L', 'L', 'L',           'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 'R'
+    );
+
+
 void keyboard_post_init_user(void) {
     // turn off default backlight
     rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
