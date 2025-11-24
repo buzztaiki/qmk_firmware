a6a902ecfaf969d5fa902eed4322b3b27bb7ae65
Taiki Sugawara <buzz.taiki@gmail.com>
2025-11-24 11:54:31 +0900

    fix indentation

--- keyboards/dailycraft/claw44/keymaps/buzztaiki/keymap.c
+++ keyboards/dailycraft/claw44/keymaps/buzztaiki/keymap.c
@@ -1,24 +1,20 @@
 /*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  Copyright 2012 Jun Wako <wakojun@gmail.com>
+  Copyright 2015 Jack Humbert
+  Copyright 2025 Taiki Sugawara
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 
 #define KC_HEN KC_INT4
 #define KC_MHEN KC_INT5
@@ -31,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
     //,--------+--------+--------+--------+--------+--------.                 ,--------+---------+--------+---------+--------+--------.
         KC_EQL , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U    , KC_I   , KC_O    , KC_P   , KC_MINS,
-     //|--------+--------+--------+--------+--------+--------|                 |--------+---------+--------+---------+--------+--------|
+    //|--------+--------+--------+--------+--------+--------|                 |--------+---------+--------+---------+--------+--------|
    LT(1,KC_GRV), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , XXXXXXX, XXXXXXX, KC_H   , KC_J    , KC_K   , KC_L    , KC_SCLN, LT(1,KC_QUOT),
     //|--------+--------+--------+--------+--------+--------|                 |--------+---------+--------+---------+--------+--------|
        MO(2)   , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , XXXXXXX, XXXXXXX, KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_RCTL,
@@ -110,7 +106,7 @@ static bool strong_hold_keycode(uint16_t keycode) {
     }
 
     switch (keycode) {
-     // pinky
+    // pinky
     case LT(1,KC_GRV):
     case LT(1,KC_QUOT):
         return true;
@@ -137,7 +133,7 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 }
 
 bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
-                       uint16_t other_keycode, keyrecord_t* other_record) {
+                      uint16_t other_keycode, keyrecord_t* other_record) {
     if (strong_hold_keycode(tap_hold_keycode)) {
         return true;
     }
