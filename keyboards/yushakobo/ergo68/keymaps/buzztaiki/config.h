63255b794f7eaf313491190dfb610ad247308706
Taiki Sugawara <buzz.taiki@gmail.com>
2026-01-05 12:15:32 +0900

    ergo68: wip

--- keyboards/yushakobo/ergo68/keymaps/buzztaiki/config.h
+++ keyboards/yushakobo/ergo68/keymaps/buzztaiki/config.h
@@ -1,4 +1,8 @@
 #pragma once
 
 // https://docs.qmk.fm/tap_hold
-#define HOLD_ON_OTHER_KEY_PRESS
+#define CHORDAL_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+
+#define TAPPING_TERM 175
+#define TAPPING_TERM_PER_KEY
